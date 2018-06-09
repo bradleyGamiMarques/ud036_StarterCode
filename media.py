2738f0cd42f313c5576d3e34d4046e85804ffc0b
Bradley Marques <bradleygamimarques@gmail.com>
2018-06-09 12:46:25 -0500

    updated the media.py class to include documentation.

--- media.py
+++ media.py
@@ -1,6 +1,8 @@
 import webbrowser
 
 class Movie():
+    """ This class defines the characteristics of a movie.
+    """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
@@ -8,4 +10,7 @@ class Movie():
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """ This function opens a web browser which navigates to the passed in
+        url.
+        """
         webbrowser.open(self.trailer_youtube_url)
